366d8fcf70a0a31cd1ecf572b82042df4e4b7e52
Camila D <camilad@Camilas-MacBook.attlocal.net>
2022-12-07 20:32:41 -0500

    test

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var wordSearch = document.getElementById("search");
 var searchHistory = document.getElementById("search-history");
 
 var word = wordSearch.value;
-
+var x =  '';
 // set searched word in local storage
 function setWordHistory(word) {
     let words = []
@@ -99,4 +99,4 @@ function findDefinition(data){
     }
 };
 
-getDefinition();
+getDefinition();t
